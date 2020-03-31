7743532d4b62ecd60f3fa7fe09e3266c31cd0937
Caleb Barynin <caleb.barynin@gmail.com>
2020-03-31 19:42:38 -0400

    Name changes (PlayerCharacter
    Changed
    any PlayerCharacter to Player
    State scripts now do not have any direct reference to the player
    IE. PlayerCharacter_StateBase is now StateBase
    
    moved ability scripts to generic folder and kept scriptable objects in original places

---
+++ WaterGame/Assets/Animation/States/Abilities/Air/Ability_Fall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace masterFeature
+{
+    [CreateAssetMenu(fileName = "New File", menuName = "Abilities/Aerial/Fall")]
+    public class Ability_Fall : StateData
+    {
+        public override void enterAbility(StateBase stateBase, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            Controller controller = stateBase.getController(animator);
+
+            // Initialise Speeds for state
+            controller.setInputSpeed(Controller.SpeedY.rise);
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        public override void updateAbility(StateBase stateBase, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            Controller controller = stateBase.getController(animator);
+
+            // Set input velocity
+            changeVelocityX(controller, animator);
+            changeVelocityY(controller, animator);
+        }
+
+        public override void exitAbility(StateBase stateBase, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+    }
+}
---
+++ WaterGame/Assets/Animation/States/Abilities/Ground/Ability_Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace masterFeature
+{
+    [CreateAssetMenu(fileName = "New File", menuName = "Abilities/Grounded/Move")]
+    public class Ability_Move : StateData
+    {
+        public override void enterAbility(StateBase stateBase, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            Controller controller = stateBase.getController(animator);
+
+            // Initialise Speeds for state
+            controller.setInputSpeed(Controller.SpeedX.run, Controller.SpeedY.idle);
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        public override void updateAbility(StateBase stateBase, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            Controller controller = stateBase.getController(animator);
+
+            // Set input velocity
+            changeVelocityX(controller, animator);
+
+            // Can return to idle
+            checkToExitMove(controller, animator);
+
+            // Can initiate Jump
+            checkToInitJump(controller, animator);
+        }
+
+        public override void exitAbility(StateBase stateBase, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+    }
+}
